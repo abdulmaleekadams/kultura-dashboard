49fad0fdd7f70511f48e57e1fbb81567c82a807c
abdulmaleekadams <harbdulmaleek@gmail.com>
2024-03-20 06:02:33 +0100

    Update root page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 'use client'
 
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 
 
 export default function Home() {
   const router = useRouter()
-
-  return router.push('/dashboard')
+  
+  router.push('/dashboard')
+  return (
+    <Link href={'/dashboard'} className="text-2xl">Visit Dashboard</Link>
+  )
   
 }
